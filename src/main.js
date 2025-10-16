85e959b607fd411aa440a286f4d611fefc5621d5
simplyyliam <simplyliam.dev@gmail.com>
2025-10-16 09:14:31 +0200

    style(navbar): add navbar

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { db } from "./lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
 document.addEventListener("DOMContentLoaded", () => {
-  const product = document.getElementById("product");
-  product.innerHTML = "asd";
+  const productList = document.getElementById("product");
   async function loadProducts() {
     const productsCol = collection(db, "products");
     const productSnapshot = await getDocs(productsCol);
     productSnapshot.forEach((doc) => {
-      console.log(doc.id, "=>", doc.data());
+      const product = doc.data()
+      const li = document.createElement('li')
+      li.textContent = product.name
+      productList.appendChild(li)
     });
   }
 
