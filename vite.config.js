2356df7963214b2a98f994a2b190e6b6776168ff
simplyyliam <simplyliam.dev@gmail.com>
2025-10-19 20:29:41 +0200

    refactor: removed / from path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-          cart: path.resolve(__dirname, "/src/pages/cart.html"),
-          checkout: path.resolve(__dirname, "/src/pages/checkout.html"),
-          login: path.resolve(__dirname, "/src/pages/login.html"),
-          products: path.resolve(__dirname, "/src/pages/products.html"),
-          main: path.resolve(__dirname, "/src/pages/index.html"),
+          cart: path.resolve(__dirname, "src/pages/cart.html"),
+          checkout: path.resolve(__dirname, "src/pages/checkout.html"),
+          login: path.resolve(__dirname, "src/pages/login.html"),
+          products: path.resolve(__dirname, "src/pages/products.html"),
+          main: path.resolve(__dirname, "src/pages/index.html"),
       }
     }
   }
