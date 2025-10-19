10501e18c1a16da679f244c63d8ae0abba318448
simplyyliam <simplyliam.dev@gmail.com>
2025-10-19 19:13:36 +0200

    refactor: moved styles folder to public, and updated link sheets in each html page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { app } from "./lib/firebase";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { initSearch } from "./lib/search";
-import "./styles/navbar.css";
-import "./styles/footer.css";
+
 
 const auth = getAuth(app);
 
