f828accfdc4bd1bab72099bf16478fa46349d050
simplyyliam <simplyliam.dev@gmail.com>
2025-10-20 08:41:38 +0200

    refactor(vite.config.js): correct main input path by removing leading slash

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-          cart: path.resolve(__dirname, "src/pages/cart.html"),
-          checkout: path.resolve(__dirname, "src/pages/checkout.html"),
-          login: path.resolve(__dirname, "src/pages/login.html"),
-          products: path.resolve(__dirname, "src/pages/products.html"),
-          main: path.resolve(__dirname, "src/pages/index.html"),
-      }
-    }
-  }
+        main: path.resolve(__dirname, "/index.html"),
+        cart: path.resolve(__dirname, "src/pages/cart.html"),
+        checkout: path.resolve(__dirname, "src/pages/checkout.html"),
+        login: path.resolve(__dirname, "src/pages/login.html"),
+        products: path.resolve(__dirname, "src/pages/products.html"),
+      },
+    },
+  },
 });
